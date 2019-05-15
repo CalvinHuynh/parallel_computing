52dc942094cdbc817a8fa2ecc86bb299dabe5ca8
RamonG92 <ramon_gill_8@hotmail.com>
2019-05-15 18:03:09 +0200

    Added the algorithm so it runs on 1 thread

--- src/main/java/nl/hva/pc/imagedenoiser/App.java
+++ src/main/java/nl/hva/pc/imagedenoiser/App.java
@@ -20,9 +20,9 @@ import org.opencv.photo.Photo;
  * @author Ramon Gill 500733062
  */
 public class App {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws InterruptedException {
         FileHelper fileHelper = new FileHelper();
-        List<Long> longList = new ArrayList<Long>();
+        List<Long> longList = new ArrayList<>();
 
         // TODO implement file download to keep the project size small
         // FileDownload fileDownload = new FileDownload();
@@ -49,40 +49,55 @@ public class App {
         // Creates a folder for the output
         fileHelper.CreateFolder("resources/image_dataset/output_images");
 
-        // Traverse the directory to retrieve the images.
-        try (Stream<Path> paths = Files.walk(Paths.get("resources/image_dataset/input_images"))) {
-            Stopwatch stopwatch = new Stopwatch();
-            paths.forEach((pathToImage) -> {
-                String nameOfImage = pathToImage.toString().substring(pathToImage.toString().lastIndexOf("/") + 1);
-                if (nameOfImage.substring(nameOfImage.lastIndexOf(".") + 1).toLowerCase().matches("jpg|png")) {
-                    // Retrieve the correct file extension from input
-                    String target = targetToReplace + "." + nameOfImage.substring(nameOfImage.lastIndexOf(".") + 1);
-                    String replacement = replacementText + "_" + templateWindowSize + "_" + wsearchWindowSize + "."
-                            + nameOfImage.substring(nameOfImage.lastIndexOf(".") + 1);
-                    String outputImageName = nameOfImage.replace(target, replacement);
+        class DenoiseImage extends Thread {
+            public synchronized void denoise() {
+                // Traverse the directory to retrieve the images.
+                try (Stream<Path> paths = Files.walk(Paths.get("resources/image_dataset/input_images"))) {
+                    Stopwatch stopwatch = new Stopwatch();
+                    paths.forEach((pathToImage) -> {
+                        String nameOfImage = pathToImage.toString().substring(pathToImage.toString().lastIndexOf("/") + 1);
+                        if (nameOfImage.substring(nameOfImage.lastIndexOf(".") + 1).toLowerCase().matches("jpg|png")) {
+                            // Retrieve the correct file extension from input
+                            String target = targetToReplace + "." + nameOfImage.substring(nameOfImage.lastIndexOf(".") + 1);
+                            String replacement = replacementText + "_" + templateWindowSize + "_" + wsearchWindowSize + "."
+                                    + nameOfImage.substring(nameOfImage.lastIndexOf(".") + 1);
+                            String outputImageName = nameOfImage.replace(target, replacement);
 
-                    // Reset stopwatch
-                    stopwatch.start();
-                    Mat source = Imgcodecs.imread(pathToImage.toString(), Imgcodecs.CV_LOAD_IMAGE_COLOR);
-                    Mat destination = new Mat(source.rows(), source.cols(), source.type());
-                    destination = source;
-                    Photo.fastNlMeansDenoisingColored(source, destination, 3, 3, templateWindowSize, wsearchWindowSize);
-                    Imgcodecs.imwrite("resources/image_dataset/output_images/" + outputImageName, destination);
-                    long elapsedTime = stopwatch.elapsedTime();
-                    System.out.println(
-                            "It took the system " + TimeUnit.MILLISECONDS.convert(elapsedTime, TimeUnit.NANOSECONDS)
-                                    + " milliseconds to denoise the image " + nameOfImage
-                                    + ". Output image has been saved with the name " + outputImageName);
-                    longList.add(elapsedTime);
-                } else {
-                    System.out.println(nameOfImage + " is not an image with the following extensions: .jpg | .png");
+                            // Reset stopwatch
+                            stopwatch.start();
+                            Mat source = Imgcodecs.imread(pathToImage.toString(), Imgcodecs.CV_LOAD_IMAGE_COLOR);
+                            Mat destination = new Mat(source.rows(), source.cols(), source.type());
+                            destination = source;
+                            Photo.fastNlMeansDenoisingColored(source, destination, 3, 3, templateWindowSize, wsearchWindowSize);
+                            Imgcodecs.imwrite("resources/image_dataset/output_images/" + outputImageName, destination);
+                            long elapsedTime = stopwatch.elapsedTime();
+                            System.out.println(
+                                    "It took the system " + TimeUnit.MILLISECONDS.convert(elapsedTime, TimeUnit.NANOSECONDS)
+                                            + " milliseconds to denoise the image " + nameOfImage
+                                            + ". Output image has been saved with the name " + outputImageName);
+                            longList.add(elapsedTime);
+                        } else {
+                            System.out.println(nameOfImage + " is not an image with the following extensions: .jpg | .png");
+                        }
+                    });
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-            });
-        } catch (IOException e) {
-            e.printStackTrace();
+                long totalTimeTaken = longList.stream().mapToLong(Long::longValue).sum();
+                System.out.println("Total time taken: " + TimeUnit.MILLISECONDS.convert(totalTimeTaken, TimeUnit.NANOSECONDS)
+                        + " milliseconds.");
+            }
         }
-        long totalTimeTaken = longList.stream().mapToLong(Long::longValue).sum();
-        System.out.println("Total time taken: " + TimeUnit.MILLISECONDS.convert(totalTimeTaken, TimeUnit.NANOSECONDS)
-                + " milliseconds.");
+        DenoiseImage thread1 = new DenoiseImage();
+        System.out.println("State for thread1 before start: " + thread1.getState());
+        System.out.println("Is alive for thread1 before start: " + thread1.isAlive());
+
+        thread1.start();
+        System.out.println("State for thread1 after start: " + thread1.getState());
+        System.out.println("Is alive for thread1 after start: " + thread1.isAlive());
+
+        thread1.denoise();
+        System.out.println("State for thread1 after denoising: " + thread1.getState());
+        System.out.println("Is alive for thread1 after denoising: " + thread1.isAlive());
     }
 }
